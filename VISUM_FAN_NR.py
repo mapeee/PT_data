f7df3a1ab76d9987f87cd5ad2f8c0b34faa15839
mapeee <marcus_peter@gmx.de>
2020-04-07 11:00:08 +0200

    initial commit

---
+++ VISUM_FAN_NR.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+"""
+
+import xlwt
+import re
+import time
+start_time = time.perf_counter()
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 120)
+
+from qgis.core import QgsDistanceArea, QgsPointXY
+distance = QgsDistanceArea()
+
+from pyproj import Proj, transform
+input_proj = Proj(init='epsg:31467') ##gauss_krueger_coordinate zone 3
+output_proj = Proj(init='epsg:25832') ##UTM zone 32N
+
+from pathlib import Path
+path = Path.home() / 'python32' / 'python_dir.txt'
+f = open(path, mode='r')
+for i in f: path = i
+path = Path.joinpath(Path(r'C:'+path),'VISUM_FAN.txt')
+f = path.read_text()
+f = f.split('\n')
+
+##connection to file
+df_FAN = pd.read_excel(r'C:'+f[0], sheet_name=f[1])
+df_VISUM = pd.read_excel(r'C:'+f[2], sheet_name=f[3])
+
+wb = xlwt.Workbook()
+ws = wb.add_sheet(f[5])
+results = 'C:'+f[4]
+
+##functions
+def dist_test(XY_FAN,XY_VISUM):
+    if len(XY_FAN) == 0:
+        return 99999, 0, 0
+    X = str(XY_FAN["GK-X"].iloc[0])
+    X = X.replace(".","")
+    X = X+"00000"
+    X = int(X[:7])
+    Y = str(XY_FAN["GK-Y"].iloc[0])
+    Y = Y.replace(".","")
+    Y = Y+"00000"
+    Y = int(Y[:7])
+    x_out, y_out = transform(input_proj, output_proj, X, Y)
+ 
+    point1 = QgsPointXY(XY_VISUM[6],XY_VISUM[7])
+    point2 = QgsPointXY(x_out,y_out)
+    dist = distance.measureLine(point1, point2)
+    return dist, x_out, y_out
+
+def writer(row,a,dist,hit):
+    ws.write(row, a+7, dist[0])
+    ws.write(row, a+5, dist[1])
+    ws.write(row, a+6, dist[2])
+    
+    #more values
+    No = hit["Master"].iloc[0]
+    if hit["Typ"].iloc[0]=="H": No = hit["HST-Nr"].iloc[0]
+    if hit["Typ"].iloc[0]=="M": No = hit["Master"].iloc[0]
+    if No==0: No = hit["HST-Nr"].iloc[0]
+    
+    ws.write(row, a+1, int(No))
+    ws.write(row, a+2, hit["Name + Ort"].iloc[0])
+    ws.write(row, a+3, hit["Alter Name"].iloc[0])
+    ws.write(row, a+4, hit["Linien"].iloc[0])
+    return
+
+##preparation
+#FAN
+undrop = ["Typ","HST-Nr","Master","DHID","GK-X","GK-Y","Name","Ortsname","Name + Ort","Alter Name", "Linien"]
+fields = list(df_FAN.columns)
+for i in undrop: fields.remove(i)
+df_FAN = df_FAN.drop(fields,axis= 1)
+df_FAN = df_FAN.fillna(0)
+df_FAN['DHID_2'] = df_FAN['DHID'].astype(str)
+for i in range(5):
+    df_FAN["DHID_2"] = df_FAN["DHID_2"].str.replace(':'+str(i+1)+':',str(i+1)+':')
+df_FAN['DHID'] = df_FAN['DHID'].astype(str)+':'
+df_FAN['Name'] = df_FAN['Name'].astype(str)
+df_FAN['Name + Ort'] = df_FAN['Name + Ort'].astype(str)
+df_FAN["Name + Ort"] = df_FAN["Name + Ort"].str.replace(',','')
+
+##insert column_names
+column = 0
+VISUM_columns = len(df_VISUM.columns)
+for i in df_VISUM.columns:
+    ws.write(0, column, i) 
+    column+=1
+ws.write(0, column, "FAN_Nr")
+ws.write(0, column+1, "FAN_Name_Ort")
+ws.write(0, column+2, "FAN_Alter_Name")
+ws.write(0, column+3, "FAN_Linien")
+ws.write(0, column+4, "FAN_X")
+ws.write(0, column+5, "FAN_Y")
+ws.write(0, column+6, "Distance")
+
+##insert_values
+df_VISUM = df_VISUM.to_numpy()
+row = 1
+for i in df_VISUM:    
+    for a in range(VISUM_columns):
+        ws.write(row, a, i[a])
+    #hits
+    hit = df_FAN[df_FAN['DHID'].str.contains(":"+str(i[2])+":")].head(1)
+    dist = dist_test(hit,i)
+    if len(hit)>0 and dist[0]<500:
+        writer(row,a,dist,hit)
+        row+=1
+        continue
+    hit = df_FAN[df_FAN['DHID_2'].str.contains(":"+str(i[2])+":")].head(1)
+    dist = dist_test(hit,i)
+    if len(hit)>0 and dist[0]<500:
+        writer(row,a,dist,hit)
+        row+=1
+        continue
+    hit = df_FAN[df_FAN['Name + Ort'].str.contains(re.escape(i[3].replace(',','')))].head(1)
+    dist = dist_test(hit,i)
+    if len(hit)>0 and dist[0]<500:
+        writer(row,a,dist,hit)
+        row+=1
+        continue
+    try: name = i[3].split(", ")[1]
+    except:
+        name = i[3]
+        row+=1
+        continue
+    name = name.replace("Abzw.","Abzweigung")
+    hit = df_FAN[df_FAN['Name'].str.contains(re.escape(name))].head(1)
+    dist = dist_test(hit,i)
+    if len(hit)>0 and dist[0]<500:
+        writer(row,a,dist,hit)
+        row+=1
+        continue
+    row+=1
+
+##output
+wb.save(results)
+print("Script finished after: "+str(int(time.perf_counter())-int(start_time))+" seconds")
---
+++ VISUM_FAN_Vol.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 21:32:49 2020
+
+@author: marcus
+"""
+
+import xlwt
+import time
+import math
+start_time = time.perf_counter()
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 120)
+
+from pathlib import Path
+path = Path.home() / 'python32' / 'python_dir.txt'
+f = open(path, mode='r')
+for i in f: path = i
+path = Path.joinpath(Path(r'C:'+path),'VISUM_Vol.txt')
+f = path.read_text()
+f = f.split('\n')
+
+##connection to file
+df_Vol = pd.read_excel(r'C:'+f[0], sheet_name = None)
+df_VISUM_FAN_Nr = pd.read_excel(r'C:'+f[1], sheet_name='VISUM_FAN').to_numpy()
+df_links = pd.read_excel(r'C:'+f[2], sheet_name='Strecken')
+
+wb = xlwt.Workbook()
+ws = wb.add_sheet("VISUM_Vol", cell_overwrite_ok=True)
+results ='C:'+f[3]
+
+
+##functions
+def string_list(df,column):
+    df[column] = df[column].astype(str)
+    df[column]= df[column].str.replace(".",",")
+    df[column]= df[column].str.split(",").apply(lambda x: [int(i) for i in x])
+    return df
+
+##preparation
+#VISUM_links
+df_links = df_links.fillna(0)
+df_links = string_list(df_links,"von")
+df_links = string_list(df_links,"nach")
+df_links['FAN_von'] = df_links['von']
+df_links['FAN_nach'] = df_links['nach']
+
+##VISUM_FAN
+for i in df_VISUM_FAN_Nr:
+    if math.isnan(i[8]) == True:continue
+    df_links['FAN_von'] = df_links['FAN_von'].apply(lambda x: [int(i[8]) if e==i[0] else e for e in x])   
+    df_links['FAN_nach'] = df_links['FAN_nach'].apply(lambda x: [int(i[8]) if e==i[0] else e for e in x])
+
+
+df_links['FAN_von'] = [list(set(b).difference(set(a))) for a, b in zip(df_links.von, df_links.FAN_von)]
+df_links['FAN_nach'] = [list(set(b).difference(set(a))) for a, b in zip(df_links.nach, df_links.FAN_nach)]
+
+
+##insert column_names and values to list
+ws.write(0, 0, "Nr")
+ws.write(0, 1, "VONKNOTNR")
+ws.write(0, 2, "NACHKNOTNR")
+ws.write(0, 3, "Vol")
+ws.write(0, 4, "Linien")
+
+t = []  ##new python list to fill in line volumes
+for i in df_links.itertuples():
+    t.append([i.strecke,i.VONKNOTNR,i.NACHKNOTNR,0,""])
+    
+##Volumes
+for name, sheet in df_Vol.items():
+    if "Kanten" in name:
+        df_Vol_line = df_Vol[name]
+        df_Vol_line = df_Vol_line.rename(columns={"Belastung MF": "Belastung_MF"})
+        for i in df_Vol_line.itertuples():
+            vol = df_links[(df_links.apply(lambda x: i.Von in x.FAN_von, axis=1))&(df_links.apply(lambda x: i.Nach in x.FAN_nach, axis=1))]
+            if len(vol)==0:
+                print (i.Von, i.Nach)
+            for row in vol.index:
+                t[row][3] = t[row][3]+i.Belastung_MF
+                try: t[row][4] = t[row][4]+str(i.Linien)
+                except:pass
+        
+        row = 1
+        for i in df_links.itertuples():    
+            ws.write(row, 0, i.strecke)
+            ws.write(row, 1, i.VONKNOTNR)
+            ws.write(row, 2, i.NACHKNOTNR)
+            ws.write(row, 3, t[row-1][3])
+            ws.write(row, 4, t[row-1][4])
+            row+=1
+    
+##output
+wb.save(results)
+print("Script finished after: "+str(int(time.perf_counter())-int(start_time))+" seconds")
