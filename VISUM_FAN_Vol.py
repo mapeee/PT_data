f7f20bc55ea4f7bb164e4cce105fb81e29fb1a9d
mapeee <marcus_peter@gmx.de>
2020-04-08 10:45:19 +0200

    additional options to analyse.

--- VISUM_FAN_Vol.py
+++ VISUM_FAN_Vol.py
@@ -4,6 +4,7 @@ Created on Sat Apr  4 21:32:49 2020
 
 @author: marcus
 """
+
 import xlwt
 import time
 import math
@@ -43,16 +44,23 @@ df_links = string_list(df_links,"von")
 df_links = string_list(df_links,"nach")
 df_links['FAN_von'] = df_links['von']
 df_links['FAN_nach'] = df_links['nach']
+df_links['HHA_von'] = df_links['von']
+df_links['HHA_nach'] = df_links['nach']
 
 ##VISUM_FAN
 for i in df_VISUM_FAN_Nr:
-    if math.isnan(i[8]) == True:continue
-    df_links['FAN_von'] = df_links['FAN_von'].apply(lambda x: [int(i[8]) if e==i[0] else e for e in x])   
-    df_links['FAN_nach'] = df_links['FAN_nach'].apply(lambda x: [int(i[8]) if e==i[0] else e for e in x])
+    if math.isnan(i[8]) == False:
+        df_links['FAN_von'] = df_links['FAN_von'].apply(lambda x: [int(i[8]) if e==i[0] else e for e in x])   
+        df_links['FAN_nach'] = df_links['FAN_nach'].apply(lambda x: [int(i[8]) if e==i[0] else e for e in x])
+    if math.isnan(i[15]) == False:
+        df_links['HHA_von'] = df_links['HHA_von'].apply(lambda x: [int(i[15]) if e==i[0] else e for e in x])   
+        df_links['HHA_nach'] = df_links['HHA_nach'].apply(lambda x: [int(i[15]) if e==i[0] else e for e in x])
 
 
 df_links['FAN_von'] = [list(set(b).difference(set(a))) for a, b in zip(df_links.von, df_links.FAN_von)]
 df_links['FAN_nach'] = [list(set(b).difference(set(a))) for a, b in zip(df_links.nach, df_links.FAN_nach)]
+df_links['HHA_von'] = [list(set(b).difference(set(a))) for a, b in zip(df_links.von, df_links.HHA_von)]
+df_links['HHA_nach'] = [list(set(b).difference(set(a))) for a, b in zip(df_links.nach, df_links.HHA_nach)]
 
 print("--join der FAN_Nummern an die Strecken erfolgreich--")
 
@@ -61,28 +69,39 @@ ws.write(0, 0, "Nr")
 ws.write(0, 1, "VONKNOTNR")
 ws.write(0, 2, "NACHKNOTNR")
 ws.write(0, 3, "Vol")
-ws.write(0, 4, "Linien")
+ws.write(0, 4, "Vol_Bus")
+ws.write(0, 5, "Vol_U")
+ws.write(0, 6, "Vol_A")
+ws.write(0, 7, "Vol_S")
+ws.write(0, 8, "Linien")
 
 t = []  ##new python list to fill in line volumes
 for i in df_links.itertuples():
-    t.append([i.strecke,i.VONKNOTNR,i.NACHKNOTNR,0,""])
+    t.append([i.strecke,i.VONKNOTNR,i.NACHKNOTNR,0,0,0,0,0,""])
  
 n = 0
+file = open('C:'+f[4],'w')
 ##Volumes
 for name, sheet in df_Vol.items():
-    if "Kanten" in name and "_U_" in name:
+    if "Kanten" in name:
         df_Vol_line = df_Vol[name]
-        df_Vol_line = df_Vol_line.rename(columns={"Belastung MF": "Belastung_MF"})
+        df_Vol_line = df_Vol_line.rename(columns={"Belastung MF": "Belastung_MF", "Von Haltestelle": "VonHst", "Nach Haltestelle":"NachHst"})
         for i in df_Vol_line.itertuples():
             n+=1
             # print("row: "+str(n))
             if n==100:pass
-            vol = df_links[(df_links.apply(lambda x: i.Von in x.FAN_von, axis=1))&(df_links.apply(lambda x: i.Nach in x.FAN_nach, axis=1))]
+            if "_U_" in name: vol = df_links[(df_links.apply(lambda x: i.Von in x.HHA_von, axis=1))&(df_links.apply(lambda x: i.Nach in x.HHA_nach, axis=1))]
+            else: vol = df_links[(df_links.apply(lambda x: i.Von in x.FAN_von, axis=1))&(df_links.apply(lambda x: i.Nach in x.FAN_nach, axis=1))]
             if len(vol)==0:
-                print (i.Von, i.Nach, i.Linien)
+                file.write(str(i.Von)+" "+str(i.Nach)+" "+i.VonHst+" "+i.NachHst+" "+str(i.Linien)+"\n")
+                print (i.Von, i.Nach, i.VonHst, i.NachHst, i.Linien)
             for row_t in vol.index:
                 t[row_t][3] = t[row_t][3]+i.Belastung_MF
-                try: t[row_t][4] = t[row_t][4]+str(i.Linien)
+                if "_Bus" in name: t[row_t][4] = t[row_t][4]+i.Belastung_MF
+                if "_U_" in name: t[row_t][5] = t[row_t][5]+i.Belastung_MF
+                if "_AKN" in name: t[row_t][6] = t[row_t][6]+i.Belastung_MF
+                if "_S_" in name: t[row_t][7] = t[row_t][7]+i.Belastung_MF
+                try: t[row_t][8] = t[row_t][8]+str(i.Linien)
                 except:pass
         
         row = 1
@@ -92,8 +111,13 @@ for name, sheet in df_Vol.items():
             ws.write(row, 2, i.NACHKNOTNR)
             ws.write(row, 3, t[row-1][3])
             ws.write(row, 4, t[row-1][4])
+            ws.write(row, 5, t[row-1][5])
+            ws.write(row, 6, t[row-1][6])
+            ws.write(row, 7, t[row-1][7])
+            ws.write(row, 8, t[row-1][8])
             row+=1
     
 ##output
+file.close()
 wb.save(results)
 print("Script finished after: "+str(int(time.perf_counter())-int(start_time))+" seconds")
